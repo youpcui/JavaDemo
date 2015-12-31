d38e2744a25b54a18497b6aa51eef81cde99fe0e
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2015-12-31 13:31:09 +0100

    update

--- src/com/ycui/demo/javademo/io/JavaRandomAccessFileIO.java
+++ src/com/ycui/demo/javademo/io/JavaRandomAccessFileIO.java
@@ -6,28 +6,28 @@ public class JavaRandomAccessFileIO {
 
 	public static void main(String args[]) throws Exception
 	{
-		RandomAccessFile raf1 = new RandomAccessFile("f:\\mldn.txt","rw") ;
+		RandomAccessFile raf1 = new RandomAccessFile("d://workspace//eclipse//JavaDemo//var//io.txt","rw") ;
 		// 随机读取有一个限制，就是说如果要进行操作，则必须指定好数据的存储长度
 		// 保存姓名（8位字符串）和年龄（int 4）：
-		String name = "zhangsan" ;
+		String name = "Pierre  " ;
 		int age = 20 ;
 		raf1.write(name.getBytes()) ;
 		raf1.writeInt(age) ;
 		
 
-		name = "lisi    " ;
+		name = "Leo     " ;
 		age = 30 ;
 		raf1.write(name.getBytes()) ;
 		raf1.writeInt(age) ;
 
-		name = "wangwu   " ;
+		name = "Tito    " ;
 		age = 33 ;
 		raf1.write(name.getBytes()) ;
 		raf1.writeInt(age) ;
 
 		raf1.close() ;
 
-		RandomAccessFile raf2 = new RandomAccessFile("f:\\mldn.txt","r") ;
+		RandomAccessFile raf2 = new RandomAccessFile("d://workspace//eclipse//JavaDemo//var//io.txt","r") ;
 		// 读取第二个人的数据
 		raf2.skipBytes(12) ;
 		byte b[] = new byte[8] ;
